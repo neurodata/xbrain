88287d41a6f0944304adadfa43710c82f913e528
evadyer <evadevah@gmail.com>
2016-04-22 14:58:08 -0500

    edits to README (data download)
    - reorganization of data folders

--- code/celldetect/OMP_ProbMap_deploy.m
+++ code/celldetect/OMP_ProbMap_deploy.m
@@ -1,19 +1,39 @@
 %%%%%%%%%%%%%%%%%%%%%%
+% OMP_ProbMap_deploy(probFile,presid,startballsz,dilatesz, kmax, paintFile, centroidFile)
+% Input
+%%%
+% probFile = string with name of the Ramon Volume containing the probability map 
+% [(Nx,Ny,Nz) array of 32 bit numbers which range from (0,1)]
+%
+% presid = stopping criterion for algorithm (the minimum correlation between
+%          probability map + template to declare a detection) [scalar from (0,1)]
+%
+% startballsz = size of sphere used as initial template [integer]
+%
+% dilatesz = dilate template by this amount when removing a cell from the
+% probability map [integer]
+%
+% kmax = maximum number of cells (alternate stopping criterion) [integer]
+%%%
+% Output
+%%%
+% paintFile = name of output file to write paint output [(Nx, Ny, Nz) array]
+% centroidFile = name of output file to write K detected centroids to [(4,K) array]
 function OMP_ProbMap_deploy(probFile,presid,startballsz,dilatesz, kmax, paintFile, centroidFile)
 
+% load in Ramon Volume 
 load(probFile) % assume stored in cube
-Prob = cube.data;
+Prob = cube.data; % Prob is now a (Nx,Ny,Nz) array of numbers from (0,1)
 
-% create sphere template inside of bounding box
-box_radius = ceil(max(startballsz)/2) + 1;
-Dict = create_synth_dict(startballsz,box_radius);
+%% Step 1. Create sphere template inside of bounding box
+box_radius = ceil(max(startballsz)/2) + 1; % define radius of bounding box around template
+Dict = create_synth_dict(startballsz,box_radius); % create dictionary containing a sphere of diameter startballsz
 Lbox = round(length(Dict)^(1/3));
 
 Nmap = zeros(size(Prob));
 newid = 1;
 newtest = Prob;
 Centroids = [];
-Confidence = [];
 for ktot = 1:kmax
     
     val = zeros(size(Dict,2),1);
