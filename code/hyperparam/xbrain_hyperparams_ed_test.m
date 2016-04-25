2524f57955a21fdfcc67fa1f40e3f62c88d5c0bb
evadyer <evadevah@gmail.com>
2016-04-25 01:05:54 -0500

    new data folder (testdata)

--- code/hyperparam/xbrain_hyperparams_ed_test.m
+++ code/hyperparam/xbrain_hyperparams_ed_test.m
@@ -1,4 +1,4 @@
-function xbrain_hyperparams_ed(pthresh, ballsz_start,ballsz_small, ballsz_large,presid, bgfg, fileName, errFile, paintOut, inData, pData, probData, mapData, nmapData, vmapData, centroidOut, outfilename)
+function xbrain_hyperparams_ed_test(pthresh, ballsz_start,ballsz_small, ballsz_large,presid, bgfg, fileName, errFile, paintOut, inData, pData, probData, mapData, nmapData, vmapData, centroidOut, outfilename)
        
 
 %% Setup
@@ -70,35 +70,10 @@ thresholdprob(probData,mapData,pthresh,minsize)
 % run vessel removal module
 removevessels(mapData,pData,nmapData,vmapData,minsize,vesseldilatesz)
 
-% segmentation error for NMap
-%tmp = load(nmapData);
-%Nmap = tmp.cube.data(1+pad(1):end-pad(1),1+pad(2):end-pad(2),1+pad(3):end-pad(3)); %padding happens here
-%segErrNMap = compute_segmentmetrics(Nmap0, Nmap);
-
-% segmentation error for Total Map (Vmap + Nmap)
-%tmp = load(vmapData);
-%Vmap = tmp.cube.data(1+pad(1):end-pad(1),1+pad(2):end-pad(2),1+pad(3):end-pad(3)); %padding happens here
-%Tmap = zeros(size(Vmap));
-%tmpid = find(Vmap); Tmap(tmpid)=1; 
-%tmpid = find(Nmap); Tmap(tmpid)=1;
-
-%Tmap0 = zeros(size(Vmap0));
-%Tmap0(find(Vmap0))=1; Tmap0(find(Nmap0))=1;
-%segErrTMap = compute_segmentmetrics(Tmap0, Tmap);
-
 % run cell finding code
 greedyspherefinder(nmapData,centroidOut,paintOut,minsize,...
     ballsz,presid,kmax, pad(1), pad(2), pad(3))
 
-% crop centroids (remove small region around zstart and zstop)
-%cropsz = 20;
-%tmp = load(centroidOut);
-%C1 = cropcentroids(tmp.Centroids,xstart+cropsz,xstop-cropsz,ystart+cropsz,ystop-cropsz,zstart+cropsz,zstop-cropsz);
-%C02 = cropcentroids(C0,xstart+cropsz,xstop-cropsz,ystart+cropsz,ystop-cropsz,zstart+cropsz,zstop-cropsz);
-%CellMetrics = compute_centroidmetrics(C02,C1,10,[300,300,100],5); 
 
-% cropped output
-%tmp = load(painOut);
-%ReconMap = tmp.cube.data(1+pad(1):end-pad(1),1+pad(2):end-pad(2),1+pad(3):end-pad(3)); %padding happens here
-
-%save(errFile, 'CellMetrics','segErrNMap','segErrTMap','fileName')
+end %%%%% end of function
+ 
